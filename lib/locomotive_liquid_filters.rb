70e66a3dc3f492cd47b9b7c92427aab9018bab6a
Daniel Hogenelst <d.a.hogenelst@gmail.com>
2019-10-06 21:25:34 +0200

    also remove requires

--- lib/locomotive_liquid_filters.rb
+++ lib/locomotive_liquid_filters.rb
@@ -1,4 +1,2 @@
-require 'locomotive_liquid_filters/date'
 require 'locomotive_liquid_filters/text'
-Liquid::Template.register_filter(LocomotiveLiquidFilters::Date)
 Liquid::Template.register_filter(LocomotiveLiquidFilters::Text)
